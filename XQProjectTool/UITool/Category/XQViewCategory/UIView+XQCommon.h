5b1b8fe55528f08a77e7f012eee7ebf7dfc89a9c
Sinking <1034439685@qq.com>
2020-05-26 18:06:24 +0800

    add UIView+XQCommon.h

---
+++ XQProjectTool/UITool/Category/XQViewCategory/UIView+XQCommon.h
@@ -0,0 +1,20 @@
+//
+//  UIView+ColorProgress.h
+//  XQLocalizedString
+//
+//  Created by WXQ on 2018/4/20.
+//  Copyright © 2018年 WangXQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (XQCommon)
+
+- (void)xq_addSubviews:(NSArray <UIView *> *)views;
+
+@end
+
+
+
+
+
---
+++ XQProjectTool/UITool/Category/XQViewCategory/UIView+XQCommon.m
@@ -0,0 +1,46 @@
+//
+//  UIView+ColorProgress.m
+//  XQLocalizedString
+//
+//  Created by WXQ on 2018/4/20.
+//  Copyright © 2018年 WangXQ. All rights reserved.
+//
+
+#import "UIView+XQCommon.h"
+
+@implementation UIView (XQCommon)
+
+- (void)xq_addSubviews:(NSArray <UIView *> *)views {
+    for (UIView *v in views) {
+        [self addSubview:v];
+    }
+}
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
